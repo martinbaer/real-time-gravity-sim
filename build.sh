3fe132748adc0b3a50556abb0fc57938ba9e0f8a
Martin Baer <martin@martinbaer.dev>
2023-03-24 19:00:43 +1000

    altered build script and changed licence to GNU GPLv3

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 rm -rf ./pkg
-wasm-pack build
+wasm-pack build --target web
 # copy all assets in ./assets/* to ./pkg
 cp -r ./assets/* ./pkg
+# copy index.html and index.js to ./pkg
+cp ./src/index.html ./pkg
+# convert app.ts to app.js and move to ./pkg
+tsc ./src/app.ts --outFile ./pkg/app.js
